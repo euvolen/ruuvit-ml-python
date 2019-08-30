d1db5e93983ab8cb7b64685f8a59ac5d72588849
volens <eugenevolen@gmail.com>
2019-08-30 21:39:09 +0300

    Influxdb library added

--- connect_influx.py
+++ connect_influx.py
@@ -1,5 +1,11 @@
-def get_userdata(link):
-    return 'data'
+from influxdb import InfluxDBClient
 
-def erase_db():
-    return 'erased'
+def get_userdata(link):
+    with InfluxDBClient(link) as client:
+        result = client.query('SELECT "data" FROM database').raw
+    return result
+
+def erase_db(link):
+    with InfluxDBClient(link) as client:
+        result = client.drop_database('database')
+    return result
